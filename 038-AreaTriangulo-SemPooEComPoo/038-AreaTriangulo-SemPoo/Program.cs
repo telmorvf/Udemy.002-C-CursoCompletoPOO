a183a745b0fd45b129eb48c725923d97dd5793f4
telmorvf <87204667+telmorvf@users.noreply.github.com>
2023-09-26 12:37:32 +0100

    01-create a new repository for Full Lessons of OOP with C#

--- 038-AreaTriangulo-SemPooEComPoo/038-AreaTriangulo-SemPoo/Program.cs
+++ 038-AreaTriangulo-SemPooEComPoo/038-AreaTriangulo-SemPoo/Program.cs
@@ -1,42 +1,45 @@
 ﻿using System;
 using System.Globalization;
 
-namespace _001_AreaTriangulo_SemPooEComPoo
+namespace _038_AreaTriangulo_SemPoo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Código sem utilizar o POO
+            // Ler os 3 lados de qualquer tipo de triangulo e apresentar a área,
+            //      utilizado a formula de Heron
+            
+            
             double xA, xB, xC, yA, yB, yC;
             Console.WriteLine("Entre com as medidas do triângulo X:");
             xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            
+
             Console.WriteLine("Entre com as medidas do triângulo Y:");
             yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            
+
             double p = (xA + xB + xC) / 2.0;
             double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
-            
+
             p = (yA + yB + yC) / 2.0;
             double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
-            
+
             Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
-            
+
             if (areaX > areaY)
             {
                 Console.WriteLine("Maior área: X");
             }
-            else {
+            else
+            {
                 Console.WriteLine("Maior área: Y");
             }
-
-
-
         }
     }
 }
