f14f3c0d1048ffab40d195922e149f34dc40d3d1
telmorvf <87204667+telmorvf@users.noreply.github.com>
2023-09-26 19:56:46 +0100

    telmo/04-Video41-MetodosTriangulo

--- 038-AreaTriangulo-SemPooEComPoo/041-AreaTriangulo-ComMetodos/Program.cs
+++ 038-AreaTriangulo-SemPooEComPoo/041-AreaTriangulo-ComMetodos/Program.cs
@@ -1,23 +1,21 @@
 ﻿using System;
 using System.Globalization;
 
-namespace _038_AreaTriangulo_ComPoo
+namespace _041_AreaTriangulo_ComMetodos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             // Código a utilizar a POO (Classes)
-            // Ler os 3 lados de qualquer tipo de triangulo e apresentar a área, utilizado a formula de Heron
-
-            // SIMPLIFICAR - Adicionar uma Classe e criar propriedades e atribuir o Objeto mem.(Stak) que é uma
-            //    referência para instanciação do objeto em mem.(Heap) onde será guardada a informação do objeto.
-            //    Podemos dizer que a referência está a apontar para null.
+            // Ler os 3 lados de qualquer tipo de triangulo e apresentar a área,
+            //      utilizado a formula de Heron
+            //  SIMPLIFICAR COM METODOS NA CLASSE
 
             Triangulo x, y;
             x = new Triangulo();
             y = new Triangulo();
-            
+
             Console.WriteLine("Entre com as medidas do triângulo X:");
             x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
@@ -28,11 +26,8 @@ namespace _038_AreaTriangulo_ComPoo
             y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double p = (x.A + x.B + x.C) / 2.0;
-            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
-
-            p = (y.A + y.B + y.C) / 2.0;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            double areaX = x.Area();
+            double areaY = y.Area();
 
             Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
