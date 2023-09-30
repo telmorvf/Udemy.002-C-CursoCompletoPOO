5a0ac56bce086b50c5497f8a87948bf6e03a07f7
telmorvf <87204667+telmorvf@users.noreply.github.com>
2023-09-30 20:53:46 +0100

    telmo/08-EndOfSection04

---
+++ 048-Exercicios-StaticMembers/048-Exercicios-StaticMembers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Runtime.ConstrainedExecution;
+
+namespace _048_Exercicios_StaticMembers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            double resultado = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
+
+            Console.WriteLine($"Valor a ser pago em reais = " 
+                + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
