3bd2e80749593ecf34a7a51aa52e3e0776a72f94
telmorvf <87204667+telmorvf@users.noreply.github.com>
2023-09-30 19:34:18 +0100

    End of static members

---
+++ 046-Class-StaticMembers_Vs_InstanceMembers/047.2-Classes-InstanceMembers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace _047._2_Classe_MembrosInstancia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+            
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double circ = calc.Circuferencia(raio);
+            double volume = calc.Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
