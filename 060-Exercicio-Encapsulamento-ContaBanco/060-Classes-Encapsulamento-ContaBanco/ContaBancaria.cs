829e7841f0e9883ef2e26f4e16db88bcd5edabce
telmorvf <87204667+telmorvf@users.noreply.github.com>
2023-10-01 19:32:54 +0100

    Fim S05 exercicio teste conta de banco até ao encapsulamneto de atributos, construtores e sobrecargas

---
+++ 060-Exercicio-Encapsulamento-ContaBanco/060-Classes-Encapsulamento-ContaBanco/ContaBancaria.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace _060_Classes_Encapsulamento_ContaBanco
+{
+    internal class ContaBancaria
+    {
+        //Propridades
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        // Construtores
+        public ContaBancaria() { }
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+        public ContaBancaria(int numero, string titular, double saldo)
+            : this (numero, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.00;
+        }
+
+        // Metodo Override Polimorfismo
+        public override string ToString()
+        {
+            return "Conta "
+            + Numero
+            + ", Titular: "
+            + Titular
+            + ", Saldo: $ "
+            + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+
+}
---
+++ 060-Exercicio-Encapsulamento-ContaBanco/060-Classes-Encapsulamento-ContaBanco/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace _060_Classes_Encapsulamento_ContaBanco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Indique o número de conta: ");
+            int conta = int.Parse(Console.ReadLine());
+
+            Console.Write("Indique o titular da conta: ");
+            string titular = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char opcao = char.Parse(Console.ReadLine());
+
+            ContaBancaria cont;
+            if (opcao == 's' || opcao == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double quantiaIn = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                cont = new ContaBancaria(conta, titular, quantiaIn);
+            }
+            else
+            {
+                cont = new ContaBancaria(conta, titular);
+            }
+
+            Console.WriteLine("\nDados da conta.");
+            Console.WriteLine(cont.ToString());
+
+            Console.Write("\nEntre um valor para o depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cont.Deposito(deposito);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(cont.ToString());
+
+            Console.Write("\nEntre um valor para o saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cont.Saque(saque);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(cont.ToString());
+        }
+    }
+}
