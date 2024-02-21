101cbd245938a8eb8c3d62877c55b1d08af95d08
Telmo Fernandes <telmorf@live.com>
2024-02-21 16:40:59 +0000

    Section 16 - End

---
+++ 226-ExtensionMethods/226-ExtensionMethod2-Cut-String/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _226_ExtensionMethod2_Cut_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1);
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
